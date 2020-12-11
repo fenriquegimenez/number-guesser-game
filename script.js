ea26b292f66bcec7b08da679e805f2e3b45097a0
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-12-11 14:35:31 -0300

    script added

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+  return Math.floor(Math.random() * 9);
+};
+const compareGuesses = (human, cpu, secret) => {
+  let humanDist = Math.abs(human - secret);
+  let cpuDist = Math.abs(cpu - secret);
+  if (humanDist < cpuDist) {
+    return true;
+  } else {
+    return false;
+  }
+};
+const updateScore = winner => {
+  if (compareGuesses() === true) {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => currentRoundNumber++;
